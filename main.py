6aa29dce393ce8671c85e91008b50b2e08ea3875
nurhge <nurhge@gmail.com>
2025-08-11 21:22:40 +0000

    Cloud

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Obtener el puerto de la variable de entorno PORT, por defecto 8080 si no está definida
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
